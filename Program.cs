e4ace92d9bc86e8830eb6f1a07fa0507d90909a0
emrahelis <emrahelis@gmail.com>
2024-10-01 21:42:51 +0300

    sds

--- Program.cs
+++ Program.cs
@@ -67,14 +67,85 @@ sortedList.Add(3,"İzmir");
 
 
 // sortedlist koleksiyonunu var ile gezmek için aşağıdaki yöntemi uygulamak gerekiyor
-foreach(var item in sortedList){
+/*foreach(var item in sortedList){
 
     var result = (DictionaryEntry)item;
     Console.WriteLine("Key : {0} Value : {1}",result.Key,result.Value);
 }
+
 
 // Var kullanmadan, key value koleksiyonunu gezmek için DictionaryEntry kullanılmalı
 foreach(DictionaryEntry item in sortedList){
 
     Console.WriteLine("Key : {0} Value : {1}",item.Key,item.Value);
 }
+*/
+
+// Öğrenciden aldığımız not sayısına göre, notları alıp, notların toplamı ve ortalamsını ekrana yazdırınız
+/*
+Console.WriteLine("Not sayisini giriniz");
+int not = int.Parse(Console.ReadLine());
+ArrayList liste = new ArrayList(not);
+
+
+for(int i =0; i < not; i++)
+{
+    Console.WriteLine("{0}. notu giriniz",i+1);
+    liste.Add(int.Parse(Console.ReadLine()));
+}
+
+// hesaplama adımlarını foreach kullanarak yapalım
+
+int toplam =0;
+
+
+// ArrayList'in içerisinde döndük
+foreach(object o in liste){
+
+    toplam+=(int)o;
+}
+Console.WriteLine("Toplam : {0} - Ortalama : {1}",toplam,toplam/liste.Count);
+*/
+
+
+// Not : foreach ile dizi içerisinde de dönebiliriz
+
+/*string[] teknoloji = {"Html","Css","Javascript","C#","Docekr","Kubernetes","Java","Rust","Go","Kotlin","Php","RabbitMQ"};
+
+foreach(string tech in teknoloji){
+
+    Console.WriteLine(tech);
+
+}
+*/
+
+// int tipinde bir dizi içerisinde dönelimö
+/*int[] plakalar ={35,06,01,48,02,81,34,35};
+foreach(int plaka in plakalar){
+    Console.WriteLine("Plaka : {0}",plaka);
+}*/
+
+// Random tipinde bir dizi yapabilir miyiz ? 
+
+// 3 adet random taşıyan bir random dizisi
+/*
+Random[] randoms ={new Random(),new Random(),new Random()};
+foreach(Random rnd in randoms){
+
+    Console.WriteLine(rnd.Next());
+}
+*/
+
+/*int[] randoms = {new Random().Next(),new Random().Next(),new Random().Next(),new Random().Next()};
+// Cevap : yukarıdaki dizi eleman sayısı kadar random değer üretilecek
+foreach(int i in rewerew)
+{
+
+    Console.WriteLine(i);
+}
+*/
+
+// Ödev : 
+// Çarpım tablosunu foreach ile yapınız(İç içe foreach)
+
+
